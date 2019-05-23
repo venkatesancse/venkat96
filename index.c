4fb0c90b71d3349cbdfa5ce71d9a2108784615d6
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-23 16:30:05 +0530

    Update index.c

--- index.c
+++ index.c
@@ -3,12 +3,20 @@ void main()
 {
 int ar[100],i,j,n;
 scanf("%d",&n);
+  if(n>1)
+  {
 for(i=0;i<n;i++)
   {
     scanf("%d",&ar[i]);
    }
  for(i=0;i<n;i++)
  {
- printf("%d \t %d \n ",ar[i],i);
+ printf("%d \t %d ",ar[i],i);
+   printf("\n");
  }
+  }
+  else
+  {
+    printf("Invalid");
+  }
  }
