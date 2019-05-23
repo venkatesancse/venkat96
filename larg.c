25d1e844ec63a7bd74a9a0bc8a21c229e5d500c5
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-23 13:21:57 +0530

    Create larg.c

---
+++ larg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[100],max,size,i,n=1;
+scanf("%d",&size);
+for(i=0;i<size;i++)
+  {
+  scanf("%d",&a[i]);
+  }
+  if(1<=size<=100000)
+  {
+  max=a[0];
+  for(i=1;i<size;i++)
+  {
+  if(a[i]>max)
+  {
+  max=a[i];
+  n=i+1;
+  }
+  }
+  printf("%d",max);
+  }
+  else
+  {
+  printf("Invalid");
+  }
