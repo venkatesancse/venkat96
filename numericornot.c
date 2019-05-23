a274be93819b743cbce102e0d4966a6a0980cd89
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-23 21:01:36 +0530

    Create numericornot.c

---
+++ numericornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+int main() 
+{ 
+    char str[100];
+    int i; 
+    gets(str);
+   if(isdigit(str[i]) != 0) 
+            {
+            printf("yes");
+            } 
+    else
+    {
+    printf("No");
+    }
+} 
