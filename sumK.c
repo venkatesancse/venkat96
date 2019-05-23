89e67f542a9e533cdceb7c7a4c6df38223b78a82
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-23 06:29:30 +0530

    Create sumK.c

---
+++ sumK.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int N,K,sumk=0;
+int ar[n];
+scanf("%d,%d",&N,&K);
+ar[]=N;
+for(int i=0;i<ar[n];i++)
+{
+printf("%d",ar[i]);
+}
+for(int j=1;j<=K;j++)
+{
+sumk=sumk+j;
+}
+printf("%d",sumk);
+}
