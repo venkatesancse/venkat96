11dafa9f2a37bde3f736c78f0caa18d552a896c8
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-24 10:44:03 +0530

    Update strcmpout.c

--- strcmpout.c
+++ strcmpout.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+#include<string.h>
 int main(void) {
 	char s1[1000],s2[1000];
 	gets(s1);
-  gets(s2);
-	if(sizeof(s1)>sizeof(s2))
+  	gets(s2);
+	if(strlen(s1)==strlen(s2))
+	{
+		printf("%s",s2);
+	}
+	else if(strlen(s1)>strlen(s2))
 	{
 		printf("%s",s1);
 	}
