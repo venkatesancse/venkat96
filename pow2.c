cf3c70ade752a2a7f7700b6f9b7b8eaa81ba258e
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-24 14:06:09 +0530

    Create pow2.c

---
+++ pow2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+	printf("yes");
+	}
+	else if(n==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
