bb6d4a1e906037e9655b5658a87e1f834fc33663
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-05-24 13:45:40 +0530

    Create minmax.c

---
+++ minmax.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int n,a[1000],i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d %d",a[0],a[n-1]);
+}
