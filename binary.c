39a11bf6753069afd2da14180f8c5b049c7e1672
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-06-22 07:45:57 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+	char str[50];
+	int K,i;
+	scanf("%s",&str);
+	K=strlen.S;
+	for(i=0;i<=K;i++)
+	{
+	 (S[i]==0 || S[i]==1)?printf("yes"):printf("no");
+	}
+}
