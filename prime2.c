f7e22143f0432e042b51ac7925f99bcc6cb7fc49
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-06-23 06:55:13 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main() 
+{
+  int n,i,c=0;
+  scanf("%d",&n);
+  for (i = 1; i <= n; i++) 
+  {
+      if (n % i == 0) 
+      {
+         c++;
+      }
+  }
+(c == 2)?printf("yes"):printf("no");
+  return 0;    
+}  
