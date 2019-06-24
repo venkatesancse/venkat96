fc8b9d152d217527d26019cc9dd1390dcb246a10
venkatesancse <50851623+venkatesancse@users.noreply.github.com>
2019-06-24 06:08:17 +0530

    Update vowelyn.c

--- vowelyn.c
+++ vowelyn.c
@@ -7,4 +7,8 @@ if ((x>= 'a' && x<= 'z') || (x>= 'A' && x<= 'Z'))
 {
 (x == 'a' || x == 'e' || x== 'i' || x == 'o' || x == 'u' || x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U') ? printf("yes"): printf(" no");
 }
+  else
+  {
+    printf("no");
+  }
 }
